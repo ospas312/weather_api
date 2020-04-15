839cc8709785c093e4be7f92f963fe9cb01fbd0b
Andrey Ospennikov <ospas312@ya.ru>
2020-04-15 20:51:24 +0300

    nine commit refactoring code

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ import {Api} from "./js/Api.js";
 const formEdit = document.forms.edit;
 const api = new Api({
     baseUrl: 'https://api.openweathermap.org/data/2.5/weather',
-    baseCity: 'Москва',
     appid: 'ff88d64ac348fdb7708ad27b0fca6420',
 });
 const weather = new Weather(api, formEdit);
-weather.mainTemp();
+weather.getTemp('Москва');
 
-formEdit.addEventListener('submit', (event) => weather.getTemp(event));
+formEdit.addEventListener('submit', (event) => {
+    event.preventDefault();
+    weather.getTemp(event.target.input.value);
+});
